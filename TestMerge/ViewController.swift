09491c39fbc87a98a6bd9964cb8a14fd78aebb57
Dmitrii Onegin <dimimimo@mail.ru>
2022-01-08 13:16:36 +0300

    Initial Commit

---
+++ TestMerge/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestMerge
+//
+//  Created by Dmitrii Onegin on 08.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
